c5d5b629a51f01c638910e2a0f25682d24d2f472
Asadul Haq Muhammad Shani <mshaniasadulhaq@gmail.com>
2023-11-15 15:54:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+console.logz = function (d) { //
+    log_file.write(util.format(d) + '\n');
+};
+
+app.get('/', (req, res) => {
+    let date_time = new Date();
+    let date = ("0" + date_time.getDate()).slice(-2);
+    let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+    let year = date_time.getFullYear();
+    let hours = date_time.getHours();
+    let minutes = date_time.getMinutes();
+    let seconds = date_time.getSeconds();
+
+    console.logz("\n============================ ")
+    console.logz(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds)
+    console.logz(req);
+    console.logz(`origin: ${req.get('origin')}`)
+    console.logz("\n============================\n")
+    res.send('Yee haw >>>')
 })
 app.listen(process.env.PORT || 3000)
