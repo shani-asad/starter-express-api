bb7949b90096ac72162f0b1e24a2b60d34412b7b
Asadul Haq Muhammad Shani <mshaniasadulhaq@gmail.com>
2023-11-15 15:55:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', { flags: 'w' });
+var log_stdout = process.stdout;
 console.logz = function (d) { //
     log_file.write(util.format(d) + '\n');
 };
